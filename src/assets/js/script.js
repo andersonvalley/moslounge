c99fcd32061bf3f82e6a8295fe352e2a1d786593
Andrey Mironov <andrey@MacBook-Pro-Andrey.local>
2021-11-19 17:32:55 +0300

    свертстаны табы, добавлена анимация на hero

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,7 +1,16 @@
 import lazyLoadInstance from './modules/lazy'
 import WOW from 'wow.js'
+import Parallax from 'parallax-js'
 
 document.addEventListener('DOMContentLoaded', () => {
   lazyLoadInstance.update()
   new WOW().init()
+
+  const scene = document.getElementById('scene')
+  const parallaxInstance = new Parallax(scene)
+
+  window.addEventListener('load', () => {
+    const preloader = document.querySelector('.preloader')
+    preloader.classList.add('preloader_hidden')
+  })
 })
