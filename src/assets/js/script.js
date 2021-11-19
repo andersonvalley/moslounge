eaf1499fd4f206e7f632aadb11c58938991b86eb
Andrey Mironov <andrey@MacBook-Pro-Andrey.local>
2021-11-19 10:54:18 +0300

    сверстана секция hero

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -4,9 +4,4 @@ import WOW from 'wow.js'
 document.addEventListener('DOMContentLoaded', () => {
   lazyLoadInstance.update()
   new WOW().init()
-  burgerMenu()
-  scrollHeader()
-  chooseAddress()
-  countNumbers()
-  interiorSlider()
 })
